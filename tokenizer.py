a297272d4a143fe3eefbddfa9faa63db55c0f848
Alpika singh <129429661+alpika0109@users.noreply.github.com>
2026-09-13 11:41:24 +0530

    Add merge function to tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -3,3 +3,6 @@ def count_pairs(tokens):
     for pair in zip(tokens,tokens[1:]):
         counts[pair] = counts.get(pair,0) + 1
     return counts
+def merge(current_tokens,most_frequent_pair,new_token):
+    new_token_list = []
+    i = 0 
