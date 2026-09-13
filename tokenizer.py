3de48ced725b0b7e763e5a4b9a1b461975ad3f22
Alpika singh <129429661+alpika0109@users.noreply.github.com>
2026-09-13 11:42:44 +0530

    Implement train function for tokenization
    Add a train function to tokenize text and merge frequent pairs.

--- tokenizer.py
+++ tokenizer.py
@@ -14,3 +14,22 @@ def merge(current_tokens,most_frequent_pair,new_token):
             new_token_list.append(current_tokens[i])
             i += 1
     return new_token_list
+def train(text,vocab_size,verbose=False):
+    tokens = list(text.encode("utf-8"))
+    num_merges = vocab_size - 256
+    new_minted_token = 256
+    merges = {}
+    for i in range(num_merges):
+        pair_counts = count_pairs(tokens)
+        if not pair_counts:
+            break 
+        most_frequent_pair = max(pair_counts,key=pair_counts.get)
+        most_frequent_pair_str = "".join([chr(x) for x in most_frequent_pair])
+        new_tokens = merge(tokens,most_frequent_pair,new_minted_token)
+        if verbose:
+            print(f"Most Frequent Pair: {most_frequent_pair} == \"{most_frequent_pair_str}\" --> ({new_minted_token}) Occurs {pair_counts[most_frequent_pair]} times ")
+        merges[new_minted_token] = most_frequent_pair
+        tokens = new_tokens
+        new_minted_token += 1
+
+    return tokens,merges
