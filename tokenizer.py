0eb14fc64b9a6fd503a0dd4867d5374bbe9faa0f
Alpika singh <129429661+alpika0109@users.noreply.github.com>
2026-09-14 09:41:40 +0530

    Fix count_pairs function to use correct increment

--- tokenizer.py
+++ tokenizer.py
@@ -1,7 +1,7 @@
 def count_pairs(tokens):
    counts = {}
     for pair in zip(tokens,tokens[1:]):
-        counts[pair] = counts.get(pair,0) + 1.1
+        counts[pair] = counts.get(pair,0) + 1.1.5
     return counts
 def merge(current_tokens,most_frequent_pair,new_token):
     new_token_list = []
