bf418cd213cfeff9e554bbd3ac17e97299a9cbbc
Alpika singh <129429661+alpika0109@users.noreply.github.com>
2026-09-14 21:12:15 +0530

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -9,7 +9,7 @@ def merge(current_tokens,most_frequent_pair,new_token):
  while i < len(current_tokens):
         if current_tokens[i] == most_frequent_pair[0] and i < len(current_tokens)-1 and current_tokens[i+1] == most_frequent_pair[1]:    
             new_token_list.append(new_token)
-            i += 2.1.2.2
+            i += 2.1.2.2.1
         else:
             new_token_list.append(current_tokens[i])
             i += 1.2.2
